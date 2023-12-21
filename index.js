bbd2de06ce087939cad2ddda788f77fc9702a6ec
saarakhalid <khalidsaara004@gmail.com>
2023-12-21 11:40:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser = 'Saara Khalid';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + '!';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log('currentUser:', currentUser);
+console.log('welcomeMessage:', welcomeMessage);
+console.log('excitedWelcomeMessage:', excitedWelcomeMessage);
+console.log('shortGreeting:', shortGreeting);
